68bab7b7e1bc288828189cc5d48d555915280c20
pragna <kadalisreepragna@gmail.com>
2026-06-06 19:24:55 +0530

    Added today's test performance practice

---
+++ src/main/java/com/telusuko/learning/SortingArray.java
@@ -0,0 +1,15 @@
+package com.telusuko.learning;
+
+import java.util.Arrays;
+
+public class SortingArray {
+	public int[] sortingArray(int[] array) {
+		for(int i=0;i<1000000;i++) {
+			Arrays.sort(array);
+			
+		}
+		return array;
+	}
+
+}
+
---
+++ src/test/java/com/telusuko/learning/TestingPerformance.java
@@ -0,0 +1,15 @@
+package com.telusuko.learning;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestingPerformance {
+
+	@Test
+	public void testSortingMethod_Performance() {
+		SortingArray array=new SortingArray();
+		int unsorted[]= {2,5,1};
+	}
+
+}
